637d98ec172f81ce67d444cbfb76332cf8c5e8d0
djraymond <djraymond@loyola.edu>
2024-10-21 12:03:40 -0400

    1

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-# Read the README.md
-# Read it again
-# Your code here
-# Delete these 4 lines of comments
+# Programmers: Theresa and Donovan
+# Course:  CS151, Dr. Zelalem Jembre Yalew
+# Due Date: 9/16/2024
+# Lab Assignment: 5
+# Problem Statement: Build a simulation of an ATM (Automatic Teller Machine), where users can view their balance,
+#deposit (e.g. add money to the account), or withdraw (e.g. take money from the account).
+# Data In: Letter of the function chosen by user
+# Data Out:  Operation of chosen function
+# Credits: In Class
