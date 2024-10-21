49ed898b2c80a7c089a1807200088793ad8e3c48
djraymond <djraymond@loyola.edu>
2024-10-21 14:12:50 -0400

    4

--- main.py
+++ main.py
@@ -11,7 +11,6 @@
 
 
 count = 0
-balance = 1000
 
 def display_menu():
         print("\nPlease select an option:"
@@ -22,18 +21,14 @@ def display_menu():
 
 def valid_num(amount):
     if not amount.isdigit():
-        print("\nPlease enter a valid number above 0.")
+        return False
     else:
-        count += 1
-        amount = int(amount)
+        int(amount)
+        return amount
 
-def valid_integer(amount):
-    if amount < 0:
-        print('error')
-    else:
-        count += 1
 
 def main():
+    balance = 1000
     print('This program operates like an ATM. You can deposit, withdraw, or view your balance.')
     choicezee = ''
 
@@ -41,35 +36,22 @@ def main():
         display_menu()
         choicezee = input('Enter your option here: ').upper().strip()
 
-    if choicezee == 'D':
-        amount = input('Enter amount of money you want deposited')
-        while amount != 2:
-            valid_num()
-            valid_integer()
-        balance = balance + amount
-        print(balance)
-        decision()
-    elif choicezee == 'V':
-        print (balance)
-        decision()
-    elif choicezee == 'W':
-        amount = input('Enter amount of money you want withdrawn')
-        while count != 2:
-            valid_num()
-            valid_integer()
-        balance = balance - amount
-        print(balance)
-        decision()
-    elif choicezee == 'E':
-        print('You are now exiting, thank you for using the program!')
+        if choicezee == 'D':
+            amount = input('Enter amount of money you want deposited ')
+            valid_num(amount)
+            balance = balance + amount
+            print(balance)
+            display_menu()
+        elif choicezee == 'V':
+            print (balance)
+            display_menu()
+
+        elif choicezee == 'W':
+            amount = input('Enter amount of money you want withdrawn')
+            valid_num(amount)
+            balance = balance - amount
+            print(balance)
+            display_menu()
+        elif choicezee == 'E':
+            print('You are now exiting, thank you for using the program!')
 main()
-
-
-
-
-
-
-
-
-
-
