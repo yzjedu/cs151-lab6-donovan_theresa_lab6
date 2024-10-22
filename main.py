5038af7eb98d31511d48fa5a20b8f17d7d2bbb04
theresadejacimo <tadejacimo@loyola.edu>
2024-10-22 11:29:33 -0400

    comments

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 # Programmers: Theresa and Donovan
 # Course:  CS151, Dr. Zelalem Jembre Yalew
 # Due Date: 9/16/2024
-# Lab Assignment: 5
+# Lab Assignment: 6
 # Problem Statement: Build a simulation of an ATM (Automatic Teller Machine), where users can view their balance,
 #deposit (e.g. add money to the account), or withdraw (e.g. take money from the account).
 # Data In: Letter of the function chosen by user
-# Data Out:  Operation of chosen function
+# Data Out:  Operation of chosen function and balance
 # Credits: In Class
 
 
-
+#Function for options of ATM
 def display_menu():
         print("\nPlease select an option:"
               "\n\t D - Deposit"
               "\n\t W - Withdraw"
               "\n\t V - View Balance"
               "\n\t E - Exit")
-
+#Function to determine if inputed value is a number and if so make it an integer
 def valid_num(amount):
     if not amount.isdigit():
         return False
     else:
         return int(amount)
 
-
+#Function containing while loops to operate the chosen option
 def main():
     balance = 1000
     print('This program operates like an ATM. You can deposit, withdraw, or view your balance.')
@@ -45,6 +45,7 @@ def main():
         elif choicezee == 'V':
             print (balance)
 
+
         elif choicezee == 'W':
             amount = input('Enter amount of money you want withdrawn')
             amount = valid_num(amount)
@@ -53,4 +54,5 @@ def main():
 
         elif choicezee == 'E':
             print('You are now exiting, thank you for using the program!')
+#Calling the main function so the code operates
 main()
